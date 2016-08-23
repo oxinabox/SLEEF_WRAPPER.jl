ded1d8cb31daac709ee2e0badb73efd8a2d05db8
Lyndon White <lyndon.white@research.uwa.edu.au>
2016-08-24 02:43:19 +1000

    =Got build.jl working

--- deps/build.jl
+++ deps/build.jl
@@ -9,24 +9,27 @@ provides(Sources, URI("http://shibatch.sourceforge.net/download/sleef-2.80.tar.g
 prefix = usrdir(sleef)
 sleefsrcdir = joinpath(srcdir(sleef), "sleef-2.80/simd")
 
-const CCOPTS = `-O -Wall -Wno-unused -Wno-attributes -lm`
-const CC = `gcc`
-const ARCHOPTS = `-DENABLE_SSE2 -msse2`
+const CCOPTS = split("-O -Wall -Wno-unused -Wno-attributes -lm")
+const ARCHOPTS = ["-DENABLE_SSE2", "-msse2"]
 
-provides(BuildProcess,
+provides(SimpleBuild,
     (@build_steps begin
         GetSources(sleef)
         @build_steps begin
             ChangeDirectory(sleefsrcdir)
-            FileRule(joinpath(libdir(sleef), "libsleef.so"),
+            FileRule(joinpath(prefix,"lib", "sleef.so"),
                 @build_steps begin
                     CreateDirectory(joinpath(prefix, "lib"))
-                    `$CC -c -fPIC $CCOPTS $ARCHOPTS sleefsimddp.c -o sleefsimddp.o`
-                    `$CC -c -fPIC $CCOPTS $ARCHOPTS sleefsimdsp.c -o sleefsimdsp.o`
-					`$CC sleefsimdsp.o sleefsimddp.o -shared -o libsleef.so`
-					`cp libsleef.so $prefix/lib/libsleef.so`
-                end)
+					#`$CC -c -fPIC $CCOPTS $ARCHOPTS sleefsimddp.c -o sleefsimddp.o`
+                    #`$CC -c -fPIC $CCOPTS $ARCHOPTS sleefsimdsp.c -o sleefsimdsp.o`
+					CCompile("sleefsimddp.c", "sleefsimddp.o", [CCOPTS; "-c"; "-fPIC"; ARCHOPTS],String[])
+					CCompile("sleefsimdsp.c", "sleefsimdsp.o", [CCOPTS; "-c"; "-fPIC"; ARCHOPTS],String[])
+					`gcc sleefsimdsp.o sleefsimddp.o -shared -o sleef.so`
+					#CCompile("sleefsimddp.o sleefsimdsp.o", "sleef.so", ["-shared"], String[] )
+					`cp sleef.so $prefix/lib/sleef.so`
+                end
+				)
         end
     end), sleef)
 
-@BinDeps.install Dict([(:sleef, :libsleef)])
+@BinDeps.install Dict([(:sleef, :jl_sleef)])
