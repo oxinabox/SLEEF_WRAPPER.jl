75e6f59c08c523c596b38a9724d2aba46481f8d0
Lyndon White <lyndon.white@research.uwa.edu.au>
2016-08-24 22:34:33 +1000

    =Domain errors for Logs

--- src/SLEEF.jl
+++ src/SLEEF.jl
@@ -62,8 +62,12 @@ function float32name(fname::Symbol)
 	Symbol(new_name)
 end
 
-function declare_1f_1f_func(fname::Symbol)
-	quote
+
+#########
+
+
+for fname in (:xsin,  :xcos,  :xtan,  :xasin,  :xacos,  :xatan,  :xexp,  :xsinh,  :xcosh,  :xtanh,  :xasinh,  :xacosh,  :xatanh,  :xcbrt,  :xexp2,  :xexp10,  :xexpm1, :xsin_u1, :xcos_u1, :xtan_u1,  :xasin_u1,  :xacos_u1,  :xatan_u1, :xcbrt_u1)
+	@eval begin
 		function $fname(a::Float64)
 			ret = ccall(($(QuoteNode(fname)), jl_sleef),VDouble,(VDouble,), a)
 			Base.convert(Float64, ret)
@@ -73,13 +77,42 @@ function declare_1f_1f_func(fname::Symbol)
 			ret = ccall(($(QuoteNode(float32name(fname))), jl_sleef),VFloat,(VFloat,), a)
 			Base.convert(Float32, ret)
 		end
-
 	end
 end
 
+for fname in (:xlog, :xlog10, :xlog_u1)
+	@eval begin
+		function $fname(a::Float64)
+			isnan(a) || a >= 0.0 || throw(DomainError())
+			ret = ccall(($(QuoteNode(fname)), jl_sleef),VDouble,(VDouble,), a)
+			Base.convert(Float64, ret)
+		end
 
-function declare_1f_2f_func(fname::Symbol)
-	quote
+		function $fname(a::Float32)
+			isnan(a) || a >= 0f0 || throw(DomainError())
+			ret = ccall(($(QuoteNode(float32name(fname))), jl_sleef),VFloat,(VFloat,), a)
+			Base.convert(Float32, ret)
+		end
+	end
+end
+
+function xlog1p(a::Float64)
+	isnan(a) || a >= -1.0 || throw(DomainError())
+	ret = ccall((:xlog1p, jl_sleef),VDouble,(VDouble,), a)
+	Base.convert(Float64, ret)
+end
+
+function xlog1p(a::Float32)
+	isnan(a) || a >= -1f0 || throw(DomainError())
+	ret = ccall((:xlog1pf, jl_sleef),VFloat,(VFloat,), a)
+	Base.convert(Float32, ret)
+end
+
+
+
+
+for fname in [:xsincos, :xsincos_u1]
+	@eval begin
 		function $fname(a::Float64)
 			ret = ccall(($(QuoteNode(fname)), jl_sleef),VDouble2,(VDouble,), a)
 			Base.convert(Tuple{Float64,Float64}, ret)
@@ -89,13 +122,12 @@ function declare_1f_2f_func(fname::Symbol)
 			ret = ccall(($(QuoteNode(float32name(fname))), jl_sleef),VFloat2,(VFloat,), a)
 			Base.convert(Tuple{Float32,Float32}, ret)
 		end
-
 	end
 end
 
 
-function declare_2f_1f_func(fname::Symbol)
-	quote
+for fname in [:xatan2, :xpow, :xatan2_u1]
+	@eval begin
 		function $fname(a::Float64, b::Float64)
 			ret = ccall(($(QuoteNode(fname)), jl_sleef),VDouble,(VDouble,VDouble), a, b)
 			Base.convert(Float64, ret)
@@ -105,23 +137,7 @@ function declare_2f_1f_func(fname::Symbol)
 			ret = ccall(($(QuoteNode(float32name(fname))), jl_sleef),VFloat,(VFloat,VFloat), a, b)
 			Base.convert(Float32, ret)
 		end
-
 	end
-end
-
-
-
-################
-for fname in [:xsin,  :xcos,  :xtan,  :xasin,  :xacos,  :xatan,  :xlog,  :xexp,  :xsinh,  :xcosh,  :xtanh,  :xasinh,  :xacosh,  :xatanh,  :xcbrt,  :xexp2,  :xexp10,  :xexpm1,  :xlog10,  :xlog1p,  :xsin_u1,  :xcos_u1,  :xtan_u1,  :xasin_u1,  :xacos_u1,  :xatan_u1,  :xlog_u1,  :xcbrt_u1]
-	eval(declare_1f_1f_func(fname))
-end
-
-for fname in [:xsincos, :xsincos_u1]
-	eval(declare_1f_2f_func(fname))
-end
-
-for fname in [:xatan2, :xpow, :xatan2_u1]
-	eval(declare_2f_1f_func(fname))
 end
 
 
--- test/basic_tests.jl
+++ test/basic_tests.jl
@@ -126,14 +126,11 @@ for T in (Float32, Float64)
     #Edge cases
     @test isinf(xlog(zero(T)))
     @test isnan(xlog(convert(T,NaN)))
-
-
     @test isinf(xlog1p(-one(T)))
     @test isnan(xlog1p(convert(T,NaN)))
+	@test_throws DomainError xlog(-one(T))
+	@test_throws DomainError xlog1p(convert(T,-2.0))
 
-	#Broken Tests
-	#@test_throws DomainError xlog(-one(T))
-	#@test_throws DomainError log1p(convert(T,-2.0))
 end
 
 
