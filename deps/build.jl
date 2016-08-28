3f87b67a7f8d8fd02ef1e352c11ff24f8539ff29
Lyndon White <lyndon.white@research.uwa.edu.au>
2016-08-28 12:25:22 +1000

    =Still trying for mac

--- deps/build.jl
+++ deps/build.jl
@@ -13,23 +13,32 @@ const CCOPTS = split("-O -Wall -Wno-unused -Wno-attributes -lm")
 const ARCHOPTS = ["-DENABLE_SSE2", "-msse2"]
 
 #HACK we call it `sleef.so` even if it is a Mac dylib
-const LIBBUILTOPTS = is_apple() ? `-dynamiclib` : `-shared`
+if true #is_apple()
+	const LIBBUILTOPTS = `-dynamiclib`
+	const LIBFN = "sleef.dynlib"
+elseif is_unix()
+	const LIBBUILTOPTS = `-shared`
+	const LIBFN = "sleef.so"
+else
+	@assert is_windows()
+	error("Windows not currently supported.")
+end
 
 provides(SimpleBuild,
     (@build_steps begin
         GetSources(sleef)
         @build_steps begin
             ChangeDirectory(sleefsrcdir)
-            FileRule(joinpath(prefix,"lib", "sleef.so"),
+            FileRule(joinpath(prefix,"lib", LIBFN),
                 @build_steps begin
                     CreateDirectory(joinpath(prefix, "lib"))
 					#`$CC -c -fPIC $CCOPTS $ARCHOPTS sleefsimddp.c -o sleefsimddp.o`
                     #`$CC -c -fPIC $CCOPTS $ARCHOPTS sleefsimdsp.c -o sleefsimdsp.o`
 					CCompile("sleefsimddp.c", "sleefsimddp.o", [CCOPTS; "-c"; "-fPIC"; ARCHOPTS],String[])
 					CCompile("sleefsimdsp.c", "sleefsimdsp.o", [CCOPTS; "-c"; "-fPIC"; ARCHOPTS],String[])
-					`gcc sleefsimdsp.o sleefsimddp.o $LIBBUILTOPTS -o sleef.so`
-					#CCompile("sleefsimddp.o sleefsimdsp.o", "sleef.so", [LIBBUILTOPTS]], String[] )
-					`cp sleef.so $prefix/lib/sleef.so`
+					`gcc $LIBBUILTOPTS -o $LIBFN sleefsimdsp.o sleefsimddp.o`
+					#CCompile("sleefsimddp.o sleefsimdsp.o",LIBFN, [LIBBUILTOPTS]], String[] )
+					`cp $LIBFN $prefix/lib/sleef.so`
                 end
 				)
         end
